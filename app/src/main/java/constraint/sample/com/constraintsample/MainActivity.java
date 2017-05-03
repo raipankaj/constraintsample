2a65b93ae4aeb10c77202b2afb39d4e22e4b6cb4
Pankaj Rai <pankaj.rai16@gmail.com>
2017-05-03 14:29:34 +0530

    Example of match_constraint added

---
+++ app/src/main/java/constraint/sample/com/constraintsample/MainActivity.java
@@ -0,0 +1,13 @@
+package constraint.sample.com.constraintsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
